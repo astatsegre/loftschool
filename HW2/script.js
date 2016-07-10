755dec592e4416248bf26d033e2fcbe5db33100a
astatsegre <astatsegre@gmail.com>
2016-07-10 19:22:52 +0300

    First version of error detection

--- HW2/script.js
+++ HW2/script.js
@@ -1,10 +1,18 @@
 var allNumbers = [1, 2, 4, 5, 6, 7, 8],
     someNumbers = [1, 2, 'привет', 4, 5, 'loftschool', 6, 7, 8],
-    noNumbers = ['это', 'массив', 'без', 'чисел'];
+    noNumbers = ['это', 'массив', 'без', 'чисел'],
+    empty = [];
 
 function isAllTrue(source, filterFn) {
     
-    for (var i = 0; i < source.length; i++) {
+    var sourceCheck = source;
+    
+    try {
+        if (sourceCheck.length == 0) {
+            throw new Error ('Пустой массив!')
+        }
+        
+        for (var i = 0; i < source.length; i++) {
         
         if (filterFn(source[i]) === false) {
             return false;
@@ -12,6 +20,11 @@ function isAllTrue(source, filterFn) {
     }
     
     return true;
+    }
+    
+    catch (e) {
+        console.log('Все очень плохо!')
+        } 
 }
 
 
@@ -22,3 +35,5 @@ return typeof val === 'number';
 console.log ( isAllTrue(allNumbers, isNumber) );
 console.log ( isAllTrue(someNumbers, isNumber) );
 console.log ( isAllTrue(noNumbers, isNumber) );
+console.log ( isAllTrue(empty, isNumber) );
+    
