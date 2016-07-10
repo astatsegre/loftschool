78975d98c6a96e2a0c79e8e1da2b23f4ee937fcd
astatsegre <astatsegre@gmail.com>
2016-07-10 18:39:52 +0300

    First commit

---
+++ HW1/script.js
@@ -0,0 +1,27 @@
+//// обычная функция
+
+
+consoleRec = function(arr, zero){
+    
+    if (zero < arr.length) {
+        console.log(arr[zero]);
+        zero++;
+        consoleRec(arr, zero);
+    }
+}
+
+consoleRec(['я', 'умею', 'писать', 'рекурсивные', 'функции'], 0);
+
+
+//// стрелочная функция
+
+consoleRec = (arr, zero) => {
+    
+    if (zero < arr.length) {
+        console.log(arr[zero]);
+        zero++;
+        consoleRec(arr, zero);
+    }
+}
+
+consoleRec(['я', 'умею', 'писать', 'рекурсивные', 'функции'], 0);
