506c956248c8afff81b380fa9f69ac831a6604f2
astatsegre <astatsegre@gmail.com>
2016-07-19 08:30:22 +0300

    started HW4

---
+++ HW4/script.js
@@ -0,0 +1,19 @@
+"use strict"
+
+let prepend = function(pasteInHere, paste) {
+    
+    let beforeMe = pasteInHere.firstElementChild;
+    
+    pasteInHere.insertBefore(paste, beforeMe);
+}
+
+//let redSquare = document.querySelector('.red-square');
+//let h1 = document.createElement('h1');
+//h1.innerHTML = 'Черный текст';
+//
+//prepend(redSquare, h1);
+
+
+let deleteTextNodes = function(element){
+    
+}
