d04daaa9e7b93b692b677c946004fad717b0cdce
astatsegre <astatsegre@gmail.com>
2016-07-19 09:22:25 +0300

    started third task

--- HW4/script.js
+++ HW4/script.js
@@ -16,4 +16,32 @@ let prepend = function(pasteInHere, paste) {
 
 let deleteTextNodes = function(element){
     
+    console.log(element.childNodes);
+    
+    for (let i = 0; i < element.childNodes.length; i++) {
+        if (element.childNodes[i].nodeType == 3) {
+            element.removeChild(element.childNodes[i]);
+        }
+    }
+    
+    console.log(element.childNodes);
 }
+
+//let body = document.body;
+//
+//deleteTextNodes(body);
+
+let deleteTextNodesRec = function(element, init) {
+    
+    if (element.childNodes[init] == 3) {
+        element.removeChild(element.childNodes[init]);
+    } else {
+        if (element.childNodes[init].childNodes.length == 0) {
+        } else {
+            init++;
+            deleteTextNodesRec(element, init);
+        }
+    }
+    
+}
+
