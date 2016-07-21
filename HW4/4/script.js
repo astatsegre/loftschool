794c1a98d2f7776dc674f5eb266c47990e239149
astatsegre <astatsegre@gmail.com>
2016-07-21 23:00:59 +0300

    Continue fourth task

--- HW4/4/script.js
+++ HW4/4/script.js
@@ -1,9 +1,29 @@
 "use strict"
 
 let scanDOM = function() {
-    
+    let searchTags = function(element, init) {
+        
+        let currentTagName = element.children[init].tagName,
+            result = {};
+        if (`${currentTagName}` == true) {
+            result[`${currentTagName}`] = `${currentTagName}` + 1;
+        } else {
+            result[`${currentTagName}`] = 1;
+        }
+        
+        
+    }
 }
 
+// let five = 'age';
+// 
+// let result = {
+//     age: 25,
+// };
+//
+//console.log(result[`${five}`]);
+//
+//console.log('age' in result);
 
 
 
