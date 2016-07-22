5863c855c38d91e336e994e754e1ded6f32d065f
astatsegre <astatsegre@gmail.com>
2016-07-22 23:56:19 +0300

    Finished function, which scan tags in scanDOM

--- HW4/4/script.js
+++ HW4/4/script.js
@@ -8,32 +8,29 @@ let scanDOM = function(element) {
             init = 0;
         }
         
+        if (element.children[init] != undefined) {
+        
+            let currentTagName = element.children[init].tagName;
+
+                    if (`${currentTagName}` in result == true) {
+                        result[`${currentTagName}`] += 1;
+                    } else {
+                        result[`${currentTagName}`] = 1;
+                    }
+        }
+        
         if (init < element.children.length) {
             
             if (element.children[init].children.length != 0) {
-                let currentTagName = element.children[init].tagName;
                 
-                if (`${currentTagName}` in result == true) {
-                    result[`${currentTagName}`] += 1;
-                } else {
-                    result[`${currentTagName}`] = 1;
-                }
                 let currentElement = element.children[init],
                     currentInit = 0;
                 searchTags(currentElement, currentInit, result);
+                init++;
+                searchTags(element, init, result);
             } else {
-            
-                let currentTagName = element.children[init].tagName;
-                
-                if (`${currentTagName}` in result == true) {
-                    result[`${currentTagName}`] += 1;
-                    init++
-                    searchTags(element, init, result);
-                } else {
-                    result[`${currentTagName}`] = 1;
-                    init++
-                    searchTags(element, init, result);
-                }
+                init++;
+                searchTags(element, init, result);
             }
         } else {
             return result;
@@ -47,19 +44,6 @@ let scanDOM = function(element) {
 
 console.log( scanDOM(document.body) );
 
-
-
-//    let scanDOM = function(element) {
-//        let result = {}
-//        let searchTags = function(element, init, result) {
-//            
-//            console.log(element, init, result);
-//        }
-//        
-//        searchTags(element, undefined, result);
-//    }
-//    
-//    scanDOM(document.body);
 
 
 
