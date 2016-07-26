18ff83197fc2d1269482c85ca84b8f9f90dae139
Stanislav <astatsegre@gmail.com>
2016-07-26 10:35:58 +0300

    finished second task

--- HW6/2/script.js
+++ HW6/2/script.js
@@ -1,39 +1,39 @@
 "use strict"
 
-//
-//let downloadCities = function() {
-//    return 
 let p = new Promise( (resolve, reject) => {
     let xhr = new XMLHttpRequest();
 
     xhr.open('GET', 'https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json');
     xhr.responseType = 'json';
-
     xhr.addEventListener('load', () => {
-        console.log(xhr.response);
+        
         resolve(xhr.response);
+        
     })
+    
     xhr.send();
-    })
+    
+})
 
 p.then(response => {
     var arrForSort = [];
     for (let i = 0; i < response.length; i++) {
         
         arrForSort[i] = response[i].name;
-
-        
-//        let div = document.createElement('div');
-//        div.innerHTML = response[i].name;
-//        document.body.appendChild(div);
     }
-    console.log(arrForSort);
+    
     arrForSort.sort();
-    console.log(arrForSort);
+    
+    for (let i = 0; i < arrForSort.length; i++) {
+        
+        let div = document.createElement('div');
+        div.innerHTML = arrForSort[i];
+        document.body.appendChild(div);
+        
+    }
     
 })
 
-//}
 
 
 
