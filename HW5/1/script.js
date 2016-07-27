acfd61115c15c22300d7a820dc1998a4cd26722e
astatsegre <astatsegre@gmail.com>
2016-07-27 21:04:55 +0300

    Fixed accordeon

--- HW5/1/script.js
+++ HW5/1/script.js
@@ -2,8 +2,6 @@
 
 let accordeon = document.querySelector(".accordeon");
 
-console.log(accordeon)
-
 accordeon.addEventListener('click', (e) => {
     e.preventDefault();
     
@@ -11,6 +9,23 @@ accordeon.addEventListener('click', (e) => {
     
     if(target.tagName != 'A') return;
     
+    let aTag = document.querySelectorAll(".accordeon a");
+    let contentList = document.querySelectorAll(".content > ul");
+    let rectangle = document.querySelectorAll(".rectangle");
+    
+    for (let i = 0; i < aTag.length; i++) {
+        aTag[i].classList.remove("active");
+    };
+    
+    for (let i = 0; i < contentList.length; i++) {
+        contentList[i].classList.remove("unhide");
+    };
+ 
+    for (let i = 0; i < rectangle.length; i++) {
+        rectangle[i].classList.remove("rectangle-active");
+    };
+ 
+ 
     e.target.classList.toggle("active");
     e.target.nextElementSibling.firstElementChild.classList.toggle("unhide");
     e.target.firstElementChild.classList.toggle("rectangle-active");
