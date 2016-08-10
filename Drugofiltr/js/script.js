556cfc10297df3abb50c16f02349dbd30a1cb416
astatsegre <astatsegre@gmail.com>
2016-08-10 15:50:50 +0300

    started adding/deleting friends functionality

--- Drugofiltr/js/script.js
+++ Drugofiltr/js/script.js
@@ -1,5 +1,6 @@
-let leftList = document.querySelector('.left-list');
-console.log(leftList);
+let leftListFriends = document.querySelector('.left-list-friends'),
+    rightListFriends = document.querySelector('.right-list-friends'),
+    searchLeft = document.querySelector('.search-left');
 
 new Promise(function(resolve, reject){
 
@@ -22,14 +23,57 @@ VK.Auth.login(response => {
         alert(response.error.error_msg);
     } else {
         console.log(response)
-        let source = document.getElementById('friendsList').innerHTML,
-            templateFn = Handlebars.compile(source),
-            template = templateFn({list: response.response});
+        let leftListSource = document.getElementById('friendsList').innerHTML,
+            leftListTemplateFn = Handlebars.compile(leftListSource),
+            leftListTemplate = leftListTemplateFn({leftList: response.response});
 
-        leftList.innerHTML = template;
+        leftListFriends.innerHTML = leftListTemplate;
+
+        let rightListSource = document.getElementById('friendsList').innerHTML,
+            rightListTemplateFn = Handlebars.compile(rightListSource),
+            rightListTemplate = rightListTemplateFn({rightList: response.response});
+
+        rightListFriends.innerHTML = rightListTemplate;
+
+        for (let i = 0; i < rightListFriends.children.length; i++) {
+//            rightListFriends.children[i].style.display = 'none';
+        }
+
     }
     })
+});
+
+searchLeft.addEventListener('input', () => {
+
+    let searchLeftValue = searchLeft.value.toLowerCase();
+
+    for (let i = 0; i < leftListFriends.children.length; i++) {
+
+        if ( leftListFriends.children[i].children[1].innerHTML.toLowerCase().indexOf(searchLeftValue) != -1 ) {
+            leftListFriends.children[i].style.display = 'block';
+        } else {
+            leftListFriends.children[i].style.display = 'none';
+        }
+    }
+});
+
+document.addEventListener('click', (e) => {
+
+    let target = e.target,
+        currentClassName = target.className;
+
+//    if (currentClassName.indexOf('icon-plus-friend') == -1) return;
+
+    let currentFriendID = target.parentNode.id;
+
+    console.log(currentFriendID);
+
+//    target.parentNode.style.display = 'none';
+
+
+
 })
+
 
 
 
