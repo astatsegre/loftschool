492995406551244208873dcf028ef63865b2380c
Stanislav <novitckas@gmail.com>
2016-08-12 13:30:48 +0300

    Continue drag and drop

--- Drugofiltr/js/script.js
+++ Drugofiltr/js/script.js
@@ -86,7 +86,6 @@ document.addEventListener('click', (e) => {
         currentClassName = target.className,
         currentFriendID = target.parentNode.dataset.friendId,
         friendsWithCurrentId = document.querySelectorAll(`div[data-friend-id='${currentFriendID}']`);
-    console.log()
 
     if (currentClassName.indexOf('icon-plus-friend') == -1 && currentClassName.indexOf('icon-cancel-friend') == -1 ) return;
 
@@ -108,13 +107,18 @@ document.addEventListener('click', (e) => {
 
 document.addEventListener('mousedown', (e) => {
     
-   
     currentElement = e.target;
-    offsetX = e.offsetX;
-    offsetY = e.offsetY;
+    
+    console.log(offsetX)
+    console.log(offsetY)
     
     if (currentElement.className != 'friend') return;
     
+    offsetX = e.offsetX;
+    offsetY = e.offsetY;
+    
+    currentElement.style.position = 'fixed';
+    currentElement.style.zIndex = '100'
     
 });
 
@@ -124,6 +128,21 @@ document.addEventListener('mouseup', (e) => {
 
 document.addEventListener('mousemove', (e) => {
     
+    if (currentElement.className != 'friend') return;
+    
+    currentElement.style.top = e.clientY - offsetY + 'px';
+    currentElement.style.left = e.clientX - offsetX + 'px';
+    
+    if (e.clientX < 215) {
+        currentElement.style.left = 215 - offsetX + 'px';
+    } else if (e.clientX > 835) {
+        currentElement.style.left = 835 - offsetX + 'px';
+        
+    } else if (e.clientY < 255) {
+        currentElement.style.top = 255 - offsetY + 'px';
+    } else if (e.clientY > 577) {
+       currentElement.style.top = 577 - offsetY + 'px'; 
+    }
 });
 
 
