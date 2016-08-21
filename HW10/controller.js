4fc8dd23c8f43c70129277509f8520dcd3acbc26
astatsegre <astatsegre@gmail.com>
2016-08-21 17:58:04 +0300

    started hw10

---
+++ HW10/controller.js
@@ -0,0 +1,24 @@
+var Controller = {
+    musicRoute: function() {
+        return Model.getMusic().then(function(music) {
+            results.innerHTML = View.render('music', {list: music});
+        });
+    },
+    friendsRoute: function() {
+        return Model.getFriends().then(function(friends) {
+            results.innerHTML = View.render('friends', {list: friends});
+        });
+    },
+    newsRoute: function() {
+        return Model.getNews().then(function(news) {
+            results.innerHTML = View.render('news', {list: news.items});
+        });
+    },
+    groupsRoute: function() {
+        return Model.getGroups().then(function(groups) {
+            console.log(groups);
+            results.innerHTML = View.render('groups', {list: groups.items}, 'v:5.53');
+        })
+    }
+
+};
