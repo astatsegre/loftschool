5b135acf445f1c1dc7586de10f5fe029ab50385c
Пользователь <>
2016-08-27 00:11:40 +0300

    COMMIT

--- HW11/script.js
+++ HW11/script.js
@@ -2,10 +2,12 @@
 
 let fs = require('fs');
 
-let dirs = fs.readdirSync('./');
+let dirs = fs.readdirSync('../../');
+
+console.log(dirs)
 
 for(let dir of dirs) {
-    let stat = fs.statSync(dir);
+//    let stat = fs.statSync(dir);
 
-    console.log(dir);
+//    console.log(dir, stat.size);
 }
